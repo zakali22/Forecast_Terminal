7abf579e146ae9a52f81c177d7775d931e54b833
Zakeria Ali <zak.fined@gmail.com>
2017-10-22 23:16:53 +0100

    Add country code

--- weather.js
+++ weather.js
@@ -4,10 +4,11 @@ const api = require('./api.json');
 const args = process.argv;
 
 let location = args[2];
+let countryCode = '';
 let temp = 0;
 
 const output = () => {
-  console.log(`${location.charAt(0).toUpperCase() + location.slice(1)} is currently ${temp} degrees celcius`);
+  console.log(`${location.charAt(0).toUpperCase() + location.slice(1)}, ${countryCode.toUpperCase()} is currently ${temp} degrees celcius`);
 };
 
 const loc = (postcode) => {
@@ -18,6 +19,7 @@ const loc = (postcode) => {
       fullBody += data.toString();
     }).on('end', () => {
       const parsed = JSON.parse(fullBody);
+      countryCode += parsed.sys.country
       temp += Math.round(parsed.main.temp - 273.15);
       output();
     });
