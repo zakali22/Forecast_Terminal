84941222218fbb9421cbac5c9970495e0a07fb27
Zakeria Ali <zak.fined@gmail.com>
2017-10-22 22:53:50 +0100

    Add functionality to the retrieval and display

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-const https = require('https');
+const http = require('http');
+const args = process.argv;
+
+let location = args[2];
+let temp = 0;
+
+const output = () => {
+  console.log(`${location.charAt(0).toUpperCase() + location.slice(1)} is currently ${temp} degrees celcius`);
+};
+
+const loc = (postcode) => {
+  const customUrl = `http://api.openweathermap.org/data/2.5/weather?q=${postcode}&APPID=17f62b6dbcccbfc5051fd5af0e7a8c05
+`;
+  http.get(customUrl, (response) => {
+    let fullBody = "";
+    response.on('data', (data) => {
+      fullBody += data.toString();
+    }).on('end', () => {
+      const parsed = JSON.parse(fullBody);
+      temp += Math.round(parsed.main.temp - 273.15);
+      output();
+    });
+  });
+};
+
+loc(location);
