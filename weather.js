72ee8c6a37883b0d5c1cd60ac2c8a3084086b7ff
Zakeria Ali <zak.fined@gmail.com>
2017-10-22 23:02:10 +0100

    Add the weather.js file

---
+++ weather.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const https = require('https');
+const api = require('./api.json');
+const args = process.argv;
+
+let location = args[2];
+let temp = 0;
+
+const output = () => {
+  console.log(`${location.charAt(0).toUpperCase() + location.slice(1)} is currently ${temp} degrees celcius`);
+};
+
+const loc = (postcode) => {
+  const customUrl = `http://api.openweathermap.org/data/2.5/weather?q=${postcode}&APPID=${api.key}`;
+  http.get(customUrl, (response) => {
+    let fullBody = "";
+    response.on('data', (data) => {
+      fullBody += data.toString();
+    }).on('end', () => {
+      const parsed = JSON.parse(fullBody);
+      temp += Math.round(parsed.main.temp - 273.15);
+      output();
+    });
+  });
+};
